6f25d00ebc75aa6752b59cdde3e3c96276b0494d
Sabit <sabit990928@gmail.com>
2024-08-23 15:26:33 +0500

    Add helper to output fixture results

---
+++ lib/clash_of_clans_fpl/fixtures/fixture_results.ex
@@ -0,0 +1,19 @@
+defmodule ClashOfClansFpl.Fixtures.FixtureResults do
+  @moduledoc """
+  Season 2024/2025.
+  """
+
+  # GW 1
+  [
+    {"Man Utd 65 - 75 Fulham"},
+    {"Ipswich 65 - 71 Liverpool"},
+    {"Arsenal 62 - 67 Wolves"},
+    {"Everton 67 - 63 Brighton"},
+    {"Newcastle 58 - 66 Southampton"},
+    {"Nott'm Forest 61 - 63 Bournemouth"},
+    {"West Ham 64 - 66 Aston Villa"},
+    {"Brentford 68 - 69 Crystal Palace"},
+    {"Chelsea 61 - 69 Man City"},
+    {"Leicester 62 - 61 Spurs"}
+  ]
+end
